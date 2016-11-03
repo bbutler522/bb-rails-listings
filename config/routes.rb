0285e3a3235d702a134bbfc89c9c30f847fa30b7
Brennan Butler <bbutler522@gmail.com>
2016-11-03 09:43:09 -0400

    Updated Routes for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
 
   root 'listings#index'
 
-  get 'signup'  => 'users#new' 
+  get '/signup'  => 'users#new' 
   resources :users
 
-  get 'login' => 'sessions#new'
+  get '/login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
